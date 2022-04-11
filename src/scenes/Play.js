f83c194addf799aa4dbc4889675bd0cca3b90383
TheRealVincentio <StephStyffe@gmail.com>
2022-04-10 19:54:28 -0700

    Full functionality

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -43,14 +43,52 @@ create() {
         frames: this.anims.generateFrameNumbers('explosion', {start: 0, end: 9, first:0}),
         frameRate: 30
     });
+    //intialize score
+    this.p1Score = 0;
+    //display score
+    let scoreConfig = {
+        fontFamily: 'Courier',
+        fontSize: '28px',
+        backgroundColor: '#F3B141',
+        color: '#843605',
+        align: 'right',
+        padding: {
+            top: 5,
+            bottom: 5,
+        },
+        fixedWidth: 100
+    }
+    this.scoreLeft = this.add.text(borderUISize+borderPadding, borderUISize +borderPadding*2, this.p1Score, scoreConfig);
+
+    // GAME OVER flag
+    this.gameOver = false;
+
+    //60 second play clock
+    scoreConfig.fixedWidth = 0;
+    this.clock = this.time.delayedCall(game.settings.gameTimer, () =>{
+        this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+        this.add.text(game.config.width/2, game.config.height/2 +64, 'Press (R) to Restart or <- for Menu',
+        scoreConfig).setOrigin(0.5);
+        this.gameOver= true;
+    }, null, this);
+        
 }
 
 update() {
+    //check key input for restart
+    if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)) {
+        this.scene.restart();
+    }
+    if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyLEFT)){
+        this.scene.start('menuScene');
+    }
     this.starfield.tilePositionX -= 4;
-    this.p1Rocket.update();
-    this.ship01.update(); // update spaceships x3
-    this.ship02.update();
-    this.ship03.update();
+    if(!this.gameOver){
+        this.p1Rocket.update();
+        this.ship01.update(); // update spaceships x3
+        this.ship02.update();
+        this.ship03.update();
+    }
     //collision checks
     if(this.checkCollision(this.p1Rocket,this.ship03)){
         this.p1Rocket.reset();
@@ -86,5 +124,9 @@ shipExplode(ship) {
         ship.alpha = 1;                 //unhide yo ships
         boom.destroy();                 // remove explosion sprite
     });
+    //score add and repaint
+    this.p1Score += ship.points;
+    this.scoreLeft.text = this.p1Score;
+    this.sound.play('sfx_explosion');
 }
 }
